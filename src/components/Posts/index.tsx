1bb620cb12319c0963a62845e1ebf4c287c63748
luna <rascaluna7@gmail.com>
2022-05-19 11:25:37 +0900

    create components

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,16 @@
+// in src/components/Posts/index.tsx
+import * as React from "react";
+import {List, Datagrid, TextField, ReferenceField} from "react-admin";
+
+export const PostList = (props:any) => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+        <ReferenceField source="userId" reference="users">
+            <TextField source="name" />
+        </ReferenceField>
+        <TextField source="id" />
+        <TextField source="title" />
+        <TextField source="body" />
+        </Datagrid>
+    </List>
+);
